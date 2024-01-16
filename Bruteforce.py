fca5debf1a0d32647f6b75bd803b63f112a950e8
Robin Robert <88583311+Tixtor710@users.noreply.github.com>
2024-01-16 19:02:50 +0530

    This is my first Hacking Code
    A simple brute force code i made with YT

---
+++ Bruteforce.py
@@ -0,0 +1,15 @@
+import random
+import string
+
+def crack_password(password):
+    attempts= 0
+    while True:
+        guess= ''.join(random.choices(string.ascii_letters + string.digits ,k=len(password)))
+        attempts +=1
+        if guess == password:
+            return attempts
+
+password = input("enter the password to crack:")
+print("Cracking the Password")
+attempts =crack_password(password)
+print(f"The Password was cracked in {attempts}attempts")
